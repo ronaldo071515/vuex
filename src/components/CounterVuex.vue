39c0f9e5b5d5fce78dfa93234dcc58eec801fccc
Ronaldo Torres <rtorres66@misena.edu.co>
2023-02-02 15:47:57 -0500

    end section vuex (state, mutations, actions, getters)

---
+++ src/components/CounterVuex.vue
@@ -0,0 +1,53 @@
+<template>
+    <h1>Counter - Vuex</h1>
+    <h2>Direct access: {{ $store.state.count }}</h2>
+    <h2>Computed: {{ countComputed }}</h2>
+
+    <button @click="increment">+1</button>
+    <button @click="incrementBy">+5</button>
+    <button @click="randomInt" :disabled="isLoading">+Random</button>
+
+    <h1>mapState</h1>
+    <h2>mapState: {{ count }}</h2>
+    <h2>lastMutation: {{ lastMutation }}</h2>
+
+
+    <h2>Direct getter: {{ $store.getters.squareCount }}</h2>
+</template>
+
+<script>
+
+import { mapState, mapActions } from 'vuex';
+
+export default {
+
+    // computed: mapState(['count'])
+
+    computed: {
+        countComputed() {
+            return this.$store.state.count
+        },
+        ...mapState(['count', 'lastMutation', 'isLoading'])
+        // ...mapState({
+        //     count: state => state.count,
+        //     lastMutation: state => state.lastMutation
+        //     lastMutation: 'lastMutation'
+        // })
+    },
+
+    methods: {
+        increment() {
+            this.$store.commit('increment')
+        },
+        incrementBy() {
+            this.$store.commit('incrementBy', 5)
+            // this.randomInt()
+        },
+        // ...mapActions(['incrementRandomInt'])
+        ...mapActions({/* le ponemos el nombre de la accion que me interesa */
+            randomInt: 'incrementRandomInt'
+        })
+    }
+
+}
+</script>
